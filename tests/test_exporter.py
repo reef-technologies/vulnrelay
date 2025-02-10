73233fd1470359f2d2637503680cdf34659c560d
João Leão <vieiraleao2005@gmail.com>
2025-02-10 05:56:07 -0300

    improved test cases for exporter

--- tests/test_exporter.py
+++ tests/test_exporter.py
@@ -8,9 +8,15 @@ from vulnrelay.metrics.metric import MetricNames
 
 def test_export_metrics():
     with tempfile.TemporaryDirectory() as temp_dir:
-        metric_exporter = MetricExporter(path=Path(temp_dir) / "test.prom")
+        path = Path(temp_dir) / "test.prom"
+        metric_exporter = MetricExporter(path=path)
+        timestamp = time.time()
         metric_exporter.save_and_export(
             values={
-                MetricNames.LAST_SCAN_AND_PUSH: time.time(),
+                MetricNames.LAST_SCAN_AND_PUSH: timestamp,
             },
         )
+
+        with open(path) as file:
+            contents: str = file.read()
+            assert contents == f"{MetricNames.LAST_SCAN_AND_PUSH.value} {timestamp}"
